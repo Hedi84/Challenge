de03357846ce4c9bb887debe259078e163ea6b51
Hedi van Meurs <alisoun@gmail.com>
2018-08-05 13:51:45 +0100

    relationships added in models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :matches
+  has_many :products, through :matches
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+  has_many :matches
+  has_many :categories, through :matches
 end
