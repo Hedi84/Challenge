e4319cb01432a523bb6fdf8484a2e04fea117bef
Hedi van Meurs <alisoun@gmail.com>
2018-08-06 16:05:08 +0100

    added uniquess of name and title

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
   has_many :matches
   has_many :products, through: :matches
+  validates :name, uniqueness: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   has_many :matches, dependent: :destroy
   has_many :categories, through: :matches
+  validates :title, uniqueness: true
 end
